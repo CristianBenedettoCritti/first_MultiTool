40560d2dfe5484b56be492b7ba433694f96aea5d
TAACRCR1 <Cristian.critti@swisscom.com>
2025-12-18 14:13:26 +0100

    added Port Scanner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import socket
 
 logo = """
 \033[38;2;0;50;255m   __  ___  __  __   __  ______   ____      ______  ____   ____    __
@@ -14,7 +15,7 @@ while True:
     os.system("cls")
     print(logo)
     print("[1] IP Lookup")
-    print("[2] Webhook")
+    print("[2] Port reader")
     print("[3] token")
     print("")
     x = input("Option: ")
@@ -28,18 +29,41 @@ while True:
         data = r.json()
         if data.get("status") == "success":
             print(f"Results for {data['query']}\n")
-            print(f"Country: {data['country']}")
-            print(f"City: {data['city']}")
-            print(f"Region: {data['region']}")
+            print(f" Country: {data['country']}")
+            print(f"  Region: {data['regionName']}")
+            print(f"    City: {data['city']}")
+            print(f"     Zip: {data['zip']}")
+            print(f"Location: {data['lat']}, {data['lon']}")
             print(f"TimeZone: {data['timezone']}")
             print("")
         else:
             print(f"Failed to load: {ip}\n")
-    pause = input("Press enter to return...")
+        pause = input("Press enter to return...")
 
     if x == "2":
         os.system("cls")
-        webhook = input("Enter Webhook: ")
+        def scan_port():
+            target = input("Enter IP: ")
+            port_range = input("Enter Port Range: ")
+            try:
+                start_port, end_port = port_range.split("-")
+                start_port = int(start_port)
+                end_port = int(end_port)
+            except ValueError:
+                print("Invalid format. Please use 'start-end' (e.g., 20-80).")
+                return
+            os.system("cls")
+            print(f"Scanning {target} from port {start_port} to {end_port}...\n")
+
+            for port in range(start_port, end_port + 1):
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(0.05)
+                result = s.connect_ex((target, port))
+                if result == 0:
+                    print(f"Port {port}: OPEN")
+                    s.close()
+            pause = input("\nPress enter to return...")
+        scan_port()
 
     if x == "3":
         os.system("cls")
